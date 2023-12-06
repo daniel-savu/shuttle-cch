b860d001c9828a9a89fa0232b38179f00b24739f
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-12-06 22:52:09 +0000

    challenge 1, task 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use axum::{routing::get, Router, http::StatusCode};
+use axum::{routing::get, Router, http::StatusCode, extract::Path};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
+}
+
+async fn bits_cube(Path((x, y)): Path<(u32, u32)>) -> String {
+    (x ^ y).pow(3).to_string()
 }
 
 async fn error_handler() -> Result<String, StatusCode> {
@@ -12,6 +16,7 @@ async fn error_handler() -> Result<String, StatusCode> {
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
     .route("/", get(hello_world))
+    .route("/1/:x/:y", get(bits_cube))
     .route("/-1/error", get(error_handler));
 
 
