c0a04c38aaed88a1d6460b1ab0a5bb6b994b1db2
Daniel Savu <23065004+daniel-savu@users.noreply.github.com>
2023-12-06 23:40:39 +0000

    challenge 1, task 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use axum::{routing::get, Router, http::StatusCode, extract::Path};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
-async fn bits_cube(Path((x, y)): Path<(u32, u32)>) -> String {
-    (x ^ y).pow(3).to_string()
+async fn bits_cube(Path(s): Path<String>) -> Result<String, StatusCode> {
+    let v: Vec<u32> = s
+        .split("/")
+        .into_iter()
+        .map(|number| u32::from_str_radix(number, 10).map_err(|_| StatusCode::BAD_REQUEST))
+        .collect::<Result<_, _>>()?;
+    Ok(v.iter().fold(0, |acc, x| acc ^ x).pow(3).to_string())
 }
 
 async fn error_handler() -> Result<String, StatusCode> {
@@ -15,10 +20,9 @@ async fn error_handler() -> Result<String, StatusCode> {
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
-    .route("/", get(hello_world))
-    .route("/1/:x/:y", get(bits_cube))
-    .route("/-1/error", get(error_handler));
-
+        .route("/", get(hello_world))
+        .route("/1/*v", get(bits_cube))
+        .route("/-1/error", get(error_handler));
 
     Ok(router.into())
 }
